149acbebb39d210081affad87c0f44c653c1b2b3
Greg Tracy <gtracy@gmail.com>
2025-05-17 11:34:21 -0500

    the pod date should be a sortable ISO date string in Dynamo

--- packages/rss_polling/app.js
+++ packages/rss_polling/app.js
@@ -112,7 +112,7 @@ exports.handler = async (event) => {
             genres : movieDetails.genres,
           
             pod_title : episode.title,
-            pod_date : episode.pubDate,
+            pod_date : new Date(episode.pubDate).toISOString(),
             pod_desc : episode.content,
             pod_guid : episode.guid, // Key
           },
